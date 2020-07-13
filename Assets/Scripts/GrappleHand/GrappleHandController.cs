e8aab7208923c102f6e64924e0d16e7b29358161
DThaiPome <r.gehrlein154@gmail.com>
2020-07-13 16:16:08 -0400

    Folders should show

--- Assets/Scripts/GrappleHand/GrappleHandController.cs
+++ Assets/Scripts/GrappleHand/GrappleHandController.cs
@@ -49,15 +49,18 @@ public class GrappleHandController : MonoBehaviour
 
     private void RestingUpdate()
     {
+        this.transform.SetParent(this.player.transform);
+
         if (Input.GetKeyDown(this.launchKey))
         {
             this.controlState = ControlState.Launching;
+            this.player.transform.DetachChildren();
         }
     }
 
     private void LaunchingUpdate()
     {
-        
+
     }
 
     private void RetractingUpdate()
@@ -66,6 +69,46 @@ public class GrappleHandController : MonoBehaviour
     }
 
     private void PullingPlayerUpdate()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        switch (this.controlState)
+        {
+            case ControlState.Resting:
+                this.RestingFixedUpdate();
+                break;
+            case ControlState.Launching:
+                this.LaunchingFixedUpdate();
+                break;
+            case ControlState.Retracting:
+                this.RetractingFixedUpdate();
+                break;
+            case ControlState.PullingPlayer:
+                this.PullingPlayerFixedUpdate();
+                break;
+        }
+    }
+
+    private void RestingFixedUpdate()
+    {
+
+    }
+
+    private void LaunchingFixedUpdate()
+    {
+        Vector3 offset = this.transform.position + (this.transform.forward * this.speed * Time.fixedDeltaTime);
+        this.rb.MovePosition(offset);
+    }
+
+    private void RetractingFixedUpdate()
+    {
+
+    }
+
+    private void PullingPlayerFixedUpdate()
     {
 
     }

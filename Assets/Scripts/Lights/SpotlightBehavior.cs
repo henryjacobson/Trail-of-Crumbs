9107234ee94f5090447d69fc1c73db7dbbaa82ca
linnielin <madisonlin5370@gmail.com>
2020-07-13 17:29:52 -0400

    Current Spotlight

--- Assets/Scripts/Lights/SpotlightBehavior.cs
+++ Assets/Scripts/Lights/SpotlightBehavior.cs
@@ -6,8 +6,8 @@ public class SpotlightBehavior : MonoBehaviour
 {
     //can do by rotation amount or by position to look at
     //for now, using empty objects as markers
-    public GameObject positionOne;
-    public GameObject positionTwo;
+    public Transform markerOne;
+    public Transform markerTwo;
 
     // in case light hits player
     public GameObject player;
@@ -23,7 +23,15 @@ public class SpotlightBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //transform.Rotate(20f * Time.deltaTime, 0, 0);
+        //transform.LookAt(markerTwo.position);
+        //Vector3.Lerp(markerOne.position, markerTwo.position, Time.deltaTime));
 
+        Vector3 dir = markerTwo.position - transform.position;
+        dir.y = 0; // keep the direction strictly horizontal
+        Quaternion rot = Quaternion.LookRotation(dir);
+        // slerp to the desired rotation over time
+        transform.rotation = Quaternion.Slerp(markerOne.rotation, markerTwo.rotation, 2.0f * Time.deltaTime);
     }
 }
 

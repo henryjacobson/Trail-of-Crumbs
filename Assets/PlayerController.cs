981701a98d941d5b31a0c9dc37f0d522ad457453
caitlinmig <caitlinmigdail@gmail.com>
2020-07-14 14:03:58 -0400

    player can now look around with camera

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 2f;
+    public float jumpAmount = 200;
+    
+    Rigidbody rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+        //rb.AddForce(transform.forward * 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * 5;
+        float moveVertical = Input.GetAxis("Vertical") * 5;
+        
+        Vector3 forceVector = rb.velocity;
+        
+        forceVector.x = moveHorizontal;
+        forceVector.y = moveVertical;
+        rb.velocity = forceVector;
+        
+        //rb.AddForce(forceVector * speed);
+        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(0, jumpAmount, 0);
+        }
+        
+        
+    }
+}

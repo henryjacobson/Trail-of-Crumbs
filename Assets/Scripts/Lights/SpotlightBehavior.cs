1d83a89982010f100a8d3892652d80e5fac43f3d
linnielin <madisonlin5370@gmail.com>
2020-07-17 17:08:05 -0400

    Added original spotlight code as comment

--- Assets/Scripts/Lights/SpotlightBehavior.cs
+++ Assets/Scripts/Lights/SpotlightBehavior.cs
@@ -66,5 +66,62 @@ public class SpotlightBehavior : MonoBehaviour
         bool wEquals = Mathf.Abs(q1.w - q2.w) < delta;
         return xEquals && yEquals && zEquals && wEquals;
     }
+
+    /*
+     * public class SpotlightBehavior : MonoBehaviour
+{
+    // x rotation values
+    public float xPosnOne = 90;
+    public float xPosnTwo = 25;
+
+    // in case light hits player
+    public Transform player;
+    // attached light object
+    //public Light spotlight;
+
+    Quaternion posnOneRot;
+    Quaternion posnTwoRot;
+    Quaternion maxRotation;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+
+        float yAngle = this.transform.localEulerAngles.y;
+        float zAngle = this.transform.localEulerAngles.z;
+
+        posnOneRot = Quaternion.Euler(new Vector3(xPosnOne, yAngle, zAngle));
+        posnTwoRot = Quaternion.Euler(new Vector3(xPosnTwo, yAngle, zAngle));
+        maxRotation = posnTwoRot;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!LevelManager.isGameOver)
+        {
+            if (transform.rotation == posnOneRot)
+            {
+                //Quaternion.Set(x, y, z, w);
+                maxRotation = posnTwoRot;
+            }
+            else if (transform.rotation == posnTwoRot)
+            {
+                maxRotation = posnOneRot;
+            }
+
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                maxRotation, Time.deltaTime);
+        } else
+        {
+            transform.LookAt(this.player);
+        }
+    }
+}
+    */
 }
 

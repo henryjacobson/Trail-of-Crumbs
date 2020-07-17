89391f3d3c7d54865ee3e0468a62762c371f5a80
DThaiPome <r.gehrlein154@gmail.com>
2020-07-16 20:14:57 -0400

    Player now hangs from grapple hand until shift is pressed again

--- Assets/Player_Movement.cs
+++ Assets/Player_Movement.cs
@@ -10,23 +10,34 @@ public class Player_Movement : MonoBehaviour
     public float gravity = -9.0f;
     
     private CharacterController _charCont;
+
+    private bool controlsActive;
     
     // Start is called before the first frame update
     void Start(){
         _charCont = GetComponent<CharacterController>();
+        controlsActive = true;
     }
 
     // Update is called once per frame
     void Update(){
-        float deltaX = Input.GetAxis("Horizontal") * speed;
-        float deltaZ = Input.GetAxis("Vertical") * speed;
-        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
-        movement = Vector3.ClampMagnitude(movement, speed);
-        
-        movement.y = gravity;
-        
-        movement *= Time.deltaTime;
-        movement = transform.TransformDirection(movement);
-        _charCont.Move(movement);
+        if (controlsActive)
+        {
+            float deltaX = Input.GetAxis("Horizontal") * speed;
+            float deltaZ = Input.GetAxis("Vertical") * speed;
+            Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+            movement = Vector3.ClampMagnitude(movement, speed);
+
+            movement.y = gravity;
+
+            movement *= Time.deltaTime;
+            movement = transform.TransformDirection(movement);
+            _charCont.Move(movement);
+        }
+    }
+
+    private void GrappleStateChanged(ControlState controlState)
+    {
+        controlsActive = controlState != ControlState.PullingPlayer;
     }
 }

fcfe7987ed79bed54f94d3885f9c9039af1a3c2e
DThaiPome <r.gehrlein154@gmail.com>
2020-07-25 16:59:51 -0400

    Powerup now has a cooldown

--- Assets/Scripts/GrappleHand/GivePowerup.cs
+++ Assets/Scripts/GrappleHand/GivePowerup.cs
@@ -8,11 +8,32 @@ public class GivePowerup : MonoBehaviour
     private PowerUp powerUp = PowerUp.ExtendedRange;
     [SerializeField]
     private float duration = 25;
+    [SerializeField]
+    private float rechargeDuration = 30;
+
+    private float rechargeTimer;
+
+    void Start()
+    {
+        this.rechargeTimer = 0;
+    }
+
+    void Update()
+    {
+        if (rechargeTimer > 0)
+        {
+            this.rechargeTimer -= Time.deltaTime;
+        } else
+        {
+            rechargeTimer = 0;
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && this.rechargeTimer > 0)
         {
+            this.rechargeTimer = this.rechargeDuration;
             GameObject.FindObjectOfType<GrappleHandController>().SetPowerUp(this.powerUp, this.duration);
         }
     }

4c4870ea283e88a96539116867705c9beee2bc8f
caitlinmig <caitlinmigdail@gmail.com>
2020-07-28 22:30:02 -0400

    added functionality for powerup timer

--- Assets/Scripts/GrappleHand/GivePowerup.cs
+++ Assets/Scripts/GrappleHand/GivePowerup.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GivePowerup : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class GivePowerup : MonoBehaviour
     private float rechargeDuration = 30;
 
     private float rechargeTimer;
+    
+    
 
     void Start()
     {
@@ -23,6 +26,21 @@ public class GivePowerup : MonoBehaviour
         if (rechargeTimer > 0)
         {
             this.rechargeTimer -= Time.deltaTime;
+
+            LevelManager lm = GameObject.FindObjectOfType<LevelManager>()
+                .GetComponent<LevelManager>();
+                
+            Transform lmTransform = lm.transform;
+            
+            GameObject powerupText;
+            
+            Transform ptt = lmTransform.GetChild(1);
+            
+            Text powerTime = ptt.GetComponent<Text>();
+            
+            powerTime.text = 
+                    rechargeTimer.ToString("f2");
+                    
         } else
         {
             rechargeTimer = 0;

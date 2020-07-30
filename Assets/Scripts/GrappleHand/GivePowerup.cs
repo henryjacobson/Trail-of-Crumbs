904dda28b9238308adbc390cf1671fc57f00cb1e
DThaiPome <r.gehrlein154@gmail.com>
2020-07-29 20:08:50 -0400

    Powerup stuff is cool

--- Assets/Scripts/GrappleHand/GivePowerup.cs
+++ Assets/Scripts/GrappleHand/GivePowerup.cs
@@ -12,8 +12,6 @@ public class GivePowerup : MonoBehaviour
     [SerializeField]
     private float rechargeDuration = 30;
     [SerializeField]
-    private GameObject powerupTimerTextObject;
-    [SerializeField]
     private GameObject powerupIndicatorObject;
 
     private float rechargeTimer;
@@ -23,10 +21,6 @@ public class GivePowerup : MonoBehaviour
     void Start()
     {
         this.rechargeTimer = 0;
-        if (powerupTimerTextObject == null)
-        {
-            this.powerupTimerTextObject = GameObject.Find("PowerupTimerText");
-        }
     }
 
     void Update()
@@ -34,16 +28,6 @@ public class GivePowerup : MonoBehaviour
         if (rechargeTimer > 0)
         {
             this.rechargeTimer -= Time.deltaTime;
-
-            LevelManager lm = GameObject.FindObjectOfType<LevelManager>()
-                .GetComponent<LevelManager>();
-                
-            Transform lmTransform = lm.transform;
-            
-            Text powerTime = powerupTimerTextObject.GetComponent<Text>();
-            
-            powerTime.text = 
-                    rechargeTimer.ToString("f2");
                     
         } else
         {

96d7c92488f8b7c6c1c4c6c380b98a6b10f76937
DThaiPome <r.gehrlein154@gmail.com>
2020-07-29 21:36:04 -0400

    Gate lock changes color now

--- Assets/Scripts/Locks/DoorLockPad.cs
+++ Assets/Scripts/Locks/DoorLockPad.cs
@@ -6,12 +6,25 @@ public class DoorLockPad : MonoBehaviour
 {
     [SerializeField]
     private DoorSlide lockedDoor;
+    [SerializeField]
+    private Color lockedColor;
+    [SerializeField]
+    private Color unlockedColor;
 
     private bool isUnlocked;
+
+    private Renderer renderer;
+
+    void Start()
+    {
+        this.renderer = this.GetComponent<Renderer>();
+    }
 
     void Update()
     {
         this.lockedDoor.enabled = this.isUnlocked;
+
+        this.renderer.material.SetColor("_EmissionColor", this.isUnlocked ? this.unlockedColor : this.lockedColor);
     }
 
     public void Unlock()

9f81ff242c28d2b90f605628d37f28862c29ea73
linnielin <madisonlin5370@gmail.com>
2020-07-30 18:38:33 -0400

    Working Locks + Updated Robots and Spotlights

--- Assets/Scripts/GrabbableObjects/KeyObject.cs
+++ Assets/Scripts/GrabbableObjects/KeyObject.cs
@@ -9,6 +9,11 @@ public class KeyObject : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.CompareTag("GrappleHand"))
+        {
+            gameObject.transform.localScale = new Vector3(5, 5, 5);
+        }
+
         if (this.doorLockPad.gameObject == other.gameObject)
         {
             this.doorLockPad.Unlock();

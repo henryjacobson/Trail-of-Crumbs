1e51507aab474bdfda9952d6389e32ee1fa77ef4
Henry <henryajacobson@gmail.com>
2020-07-29 20:07:14 -0500

    cell prime behavior

--- Assets/Scripts/CellPrimeBehavior.cs
+++ Assets/Scripts/CellPrimeBehavior.cs
@@ -9,31 +9,72 @@ public class CellPrimeBehavior : MonoBehaviour
     Transform player;
     bool following;
     float playerHeightOffset;
+    Animator anim;
+    CharacterController playerController;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerController = player.GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
         following = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        FacePlayer();
         if(following)
         {
             var self2d = new Vector2(transform.position.x, transform.position.z);
             var player2d = new Vector2(player.position.x, player.position.z);
+
+            UpdateAnimation(self2d, player2d);
+
             if (Vector2.Distance(self2d, player2d) > followDistance)
             {
                 var offset = (self2d - player2d).normalized * followDistance;
                 var target = player.position + new Vector3(offset.x, playerHeightOffset, offset.y);
                 transform.position = target;
             }
+            else
+            {
+                var pos = transform.position;
+                transform.position = new Vector3(pos.x, player.position.y + playerHeightOffset, pos.z);
+            }
+        }
+        else
+        {
+            anim.SetInteger("State", 3);
         }
         if (Mathf.Abs(transform.position.x) > 60 || Mathf.Abs(transform.position.z) > 60)
         {
             FindObjectOfType<LevelManager>().LevelWon();
         }
+    }
+
+    void UpdateAnimation(Vector2 self2d, Vector2 player2d)
+    {
+        if (!playerController.isGrounded)
+        {
+            anim.SetInteger("State", 2);
+        }
+        else if (Vector2.Distance(self2d, player2d) > followDistance)
+        {
+            anim.SetInteger("State", 1);
+        }
+        else
+        {
+            anim.SetInteger("State", 0);
+        }
+    }
+
+    void FacePlayer()
+    {
+        Vector3 lookRotation = player.position - transform.position;
+        lookRotation.y = 0;
+        Quaternion lookQuat = Quaternion.LookRotation(lookRotation);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookQuat, Time.deltaTime * 5);
     }
 
     private void OnTriggerEnter(Collider other)

3df297029b49a950477350bb848a9f125e696900
caitlinmig <caitlinmigdail@gmail.com>
2020-08-04 15:17:07 -0400

    added laser but it doesn't move with the hand yet

---
+++ Assets/Laser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//citation: https://www.youtube.com/watch?v=kzHNUT9q4JE tutorial
+public class Laser : MonoBehaviour
+{
+    private LineRenderer lr;
+    // initialization 
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lr.SetPosition(0, transform.position);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider)
+            {
+                lr.SetPosition(1, hit.point);
+            }
+        }
+        else lr.SetPosition(1, transform.forward*5000);
+    }
+}

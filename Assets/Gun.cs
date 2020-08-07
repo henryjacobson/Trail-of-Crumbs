9a15766c233d33c117e9301824aba07c29f76fc0
caitlinmig <caitlinmigdail@gmail.com>
2020-08-07 01:05:17 -0400

    laser moves with hand now

---
+++ Assets/Gun.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+//used tutorial https://www.youtube.com/watch?v=THnivyG0Mvo
+public class Gun : MonoBehaviour {
+    
+    public float damage = 10f;
+    public float range = 100f;
+    
+    public Camera fpsCam;
+    public ParticleSystem laserFlash;
+
+    // Update is called once per frame
+    void Update() {
+        
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Shoot();
+        }
+        
+    }
+    
+    void Shoot ()
+    {
+        laserFlash.Play();
+        
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}

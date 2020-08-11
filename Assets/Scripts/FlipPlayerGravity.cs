c9b3d307723e523d1d2652babaa966246f16bbdf
DThaiPome <r.gehrlein154@gmail.com>
2020-08-11 15:41:42 -0400

    Gravity field has a prefab and SFX that are probably not annoying

--- Assets/Scripts/FlipPlayerGravity.cs
+++ Assets/Scripts/FlipPlayerGravity.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class FlipPlayerGravity : MonoBehaviour
 {
+    [SerializeField]
+    private AudioClip flipSFX;
+    [SerializeField]
+    private AudioClip unflipSFX;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            if (this.flipSFX != null)
+            {
+                this.Play(this.flipSFX);
+            }
+
             other.SendMessage("FlipGravity");
         }
     }
@@ -17,7 +26,17 @@ public class FlipPlayerGravity : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            if (this.unflipSFX != null)
+            {
+                this.Play(this.unflipSFX);
+            }
+
             other.SendMessage("FlipGravity");
         }
+    }
+
+    private void Play(AudioClip clip)
+    {
+        AudioSource.PlayClipAtPoint(clip, Camera.main.transform.position);
     }
 }

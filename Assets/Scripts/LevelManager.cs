78ef8854a0ec17de30c74b9b130d577d33e0e174
linnielin <madisonlin5370@gmail.com>
2020-08-11 15:47:06 -0400

    Update LevelManager.cs

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class LevelManager : MonoBehaviour
 {

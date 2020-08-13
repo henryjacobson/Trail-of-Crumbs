08853ca3b77253135442f6e6a808e0fe6564f341
DThaiPome <r.gehrlein154@gmail.com>
2020-08-13 14:40:03 -0400

    Cleaned up password interface scripts

--- Assets/Scripts/Password/PasswordInterface.cs
+++ Assets/Scripts/Password/PasswordInterface.cs
@@ -10,21 +10,32 @@ public class PasswordInterface : MonoBehaviour
     private TMP_InputField inputField;
     [SerializeField]
     private float distanceToActivate = 2;
+    [SerializeField]
+    private int passwordLength = 4;
 
     private Transform player;
+
+    private bool solved;
 
     void Start()
     {
         this.player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        LevelManager.onLevelReset += this.Reset;
+    }
+
+    void OnDestroy()
+    {
+        LevelManager.onLevelReset -= this.Reset;
     }
 
     void Update()
     {
-        if (Vector3.Distance(this.player.position, this.transform.position) <= this.distanceToActivate) {
-            this.inputField.ActivateInputField();
-        } else
+        this.SetInputEnabled();
+        this.LockSelection();
+        if (!this.solved)
         {
-            this.inputField.DeactivateInputField();
+            this.CheckPasswordSolved();
         }
     }
 
@@ -32,5 +43,50 @@ public class PasswordInterface : MonoBehaviour
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(this.transform.position, this.distanceToActivate);
+    }
+
+    private void CheckPasswordSolved()
+    {
+        string text = this.inputField.text;
+        if (text.Length >= this.passwordLength)
+        {
+            if (FindObjectOfType<PasswordManager>().VerifyPassword(text))
+            {
+                this.solved = true;
+            } else
+            {
+                Debug.Log(text + " " + FindObjectOfType<PasswordManager>().GetPassword());
+                this.Reset();
+            }
+        }
+    }
+
+    private void Reset()
+    {
+        this.inputField.text = "";
+    }
+
+    private void LockSelection()
+    {
+        this.inputField.caretPosition = this.inputField.text.Length;
+    }
+
+    private void SetInputEnabled()
+    {
+        if (this.CanUseInterface())
+        {
+            this.inputField.ActivateInputField();
+        }
+        else
+        {
+            this.inputField.DeactivateInputField();
+        }
+    }
+
+    private bool CanUseInterface()
+    {
+        return !this.solved 
+            && Vector3.Distance(this.player.position, this.transform.position) <= this.distanceToActivate
+            && this.inputField.text.Length < this.passwordLength;
     }
 }

9e363c1290ca0f3c1723bd472c9820b75712813f
DThaiPome <r.gehrlein154@gmail.com>
2020-08-12 23:35:05 -0400

    Added password manager to level manager prefab

---
+++ Assets/Scripts/Password/PasswordManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PasswordManager : MonoBehaviour
+{
+    [SerializeField]
+    private string password = "";
+    [SerializeField]
+    private int randomPasswordLength = 4;
+
+    void Start()
+    {
+        if (this.password == "")
+        {
+            this.password = GetRandomPassword(this.randomPasswordLength);
+        }
+    }
+
+    private static string GetRandomPassword(int length)
+    {
+        return ((int)Random.Range(0, Mathf.Pow(10, length))).ToString();
+    }
+
+    void Update()
+    {
+
+    }
+}

f8c5a96400016c08b6451a7c8a75a1f44af2fc6e
DThaiPome <r.gehrlein154@gmail.com>
2020-08-12 20:44:53 -0400

    Added objects with colored lights, where only one color light is enabled at a time

---
+++ Assets/Scripts/ColorDrone/ColorFlipper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorFlipper : MonoBehaviour
+{
+    public static ColorDroneSpotlightColor activeColor;
+
+    void Start()
+    {
+        activeColor = ColorDroneSpotlightColor.Red;
+    }
+
+    public static void FlipColor()
+    {
+        switch(activeColor)
+        {
+            case ColorDroneSpotlightColor.Red:
+                activeColor = ColorDroneSpotlightColor.Blue;
+                break;
+            case ColorDroneSpotlightColor.Blue:
+                activeColor = ColorDroneSpotlightColor.Red;
+                break;
+            default:
+                return;
+        }
+    }
+}
---
+++ Assets/Scripts/ColorDrone/FlipColorOnEnter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipColorOnEnter : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ColorFlipper.FlipColor();
+        }
+    }
+}

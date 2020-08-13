81d5f832940449e4d59bbc729adc62918db9892f
DThaiPome <r.gehrlein154@gmail.com>
2020-08-13 09:04:51 -0400

    Password interface that allows typing from a certain distance

---
+++ Assets/Scripts/Password/PasswordInterface.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PasswordInterface : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_InputField inputField;
+    [SerializeField]
+    private float distanceToActivate = 2;
+
+    private Transform player;
+
+    void Start()
+    {
+        this.player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(this.player.position, this.transform.position) <= this.distanceToActivate) {
+            this.inputField.ActivateInputField();
+        } else
+        {
+            this.inputField.DeactivateInputField();
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(this.transform.position, this.distanceToActivate);
+    }
+}

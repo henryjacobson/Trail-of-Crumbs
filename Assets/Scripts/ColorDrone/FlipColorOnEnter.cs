16b5caa526fd3b054bf3b3aeaf126d9f409f1cbf
DThaiPome <r.gehrlein154@gmail.com>
2020-08-13 23:58:22 -0400

    Color flipper changes color wow

--- Assets/Scripts/ColorDrone/FlipColorOnEnter.cs
+++ Assets/Scripts/ColorDrone/FlipColorOnEnter.cs
@@ -4,11 +4,47 @@ using UnityEngine;
 
 public class FlipColorOnEnter : MonoBehaviour
 {
+    [SerializeField]
+    private AudioClip flipSFX;
+
+    private Renderer renderer;
+
+    void Start()
+    {
+        this.renderer = this.GetComponent<Renderer>();
+
+        this.SetColor();
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") || other.CompareTag("GrappleHand"))
         {
-            ColorFlipper.FlipColor();
+            this.OnFlip();
+        }
+    }
+
+    private void OnFlip()
+    {
+        AudioSource.PlayClipAtPoint(this.flipSFX, this.transform.position);
+
+        ColorFlipper.FlipColor();
+
+        this.SetColor();
+    }
+
+    private void SetColor()
+    {
+        switch (ColorFlipper.activeColor)
+        {
+            case ColorDroneSpotlightColor.Blue:
+                this.renderer.material.color = Color.cyan;
+                break;
+            case ColorDroneSpotlightColor.Red:
+                this.renderer.material.color = Color.red;
+                break;
+            default:
+                return;
         }
     }
 }

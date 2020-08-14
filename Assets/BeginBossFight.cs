e1c008f79e592418ae69e1b3ad67fab72806f6f8
Henry <henryajacobson@gmail.com>
2020-08-13 22:29:53 -0500

    most boss stuff

---
+++ Assets/BeginBossFight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeginBossFight : MonoBehaviour
+{
+    DisableControls controls;
+    CrumbsBanksAI boss;
+
+    private void Start()
+    {
+        boss = FindObjectOfType<CrumbsBanksAI>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        boss.SpotPlayer();
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/PreBossBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreBossBehavior : MonoBehaviour
+{
+    public GameObject preBoss;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Instantiate(preBoss, other.gameObject.transform);
+
+            Camera.main.GetComponent<AudioSource>().clip = null;
+
+            Destroy(gameObject);
+        }
+    }
+}

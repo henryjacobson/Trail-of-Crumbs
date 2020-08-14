754d2335d3440d7ebfa53436163bd878e87ac83f
DThaiPome <r.gehrlein154@gmail.com>
2020-08-14 14:36:56 -0400

    Boss room but cooler

---
+++ Assets/LoseOnContact.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseOnContact : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            FindObjectOfType<LevelManager>().LevelLost();
+        }
+    }
+}
